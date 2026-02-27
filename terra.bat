30f4240bd29e7504cf55aca66f0872689717193e
Francisco <franbecpin@gmail.com>
2026-02-27 17:46:17 +0100

    fea: script para ejecutar Terraform desde otro directorio

---
+++ terra.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+setlocal
+
+REM Ruta del ejecutable de Terraform
+set "TERRAFORM_EXE=C:\Users\francisco.becerra\Desktop\DESARROLLO\FORMACION\UNIR\Servers\terraform\terraform.exe"
+
+REM Ruta del proyecto Terraform
+set "TERRAFORM_DIR=C:\Users\francisco.becerra\Desktop\DESARROLLO\FORMACION\UNIR\Servers\terraform"
+
+echo Ejecutando Terraform en: %TERRAFORM_DIR%
+echo Comando: "%TERRAFORM_EXE%" -chdir="%TERRAFORM_DIR%" %*
+
+"%TERRAFORM_EXE%" -chdir="%TERRAFORM_DIR%" %*
+
+echo.
+echo ---- Fin de la ejecución de Terraform ----
+endlocal
+pause
---
+++ terra.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/bash
+
+# -----------------------------------------------------------
+# Script para ejecutar Terraform sin cambiar el directorio
+# actual, usando -chdir=<ruta>.
+# -----------------------------------------------------------
+
+# Ruta del directorio donde tienes tu proyecto Terraform
+TERRAFORM_DIR="/home/francisco.becerra/Servers/terraform"
+
+# Ruta del ejecutable de Terraform
+# Si Terraform está en el PATH, usa simplemente: TERRAFORM_EXE="terraform"
+TERRAFORM_EXE="$TERRAFORM_DIR/terraform"
+
+echo "Ejecutando Terraform en: $TERRAFORM_DIR"
+echo "Comando: \"$TERRAFORM_EXE\" -chdir=\"$TERRAFORM_DIR\" $@"
+
+"$TERRAFORM_EXE" -chdir="$TERRAFORM_DIR" "$@"
+
+echo "---- Fin de la ejecución de Terraform ----"
